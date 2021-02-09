79ba363134259cf0c5c889021136cf816428f947
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-09 17:18:42 +0000

    book has title passed

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,3 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
 
+    private Book book1;
+    private Book book2;
+    private Book book3;
+    
+    @Before
+    public void before(){
+        book1 = new Book("The Davinci Code", "Dan Brown", "Thriller");
+    }
+    
+    @Test
+    public void hasTitle(){
+        assertEquals("The Davinci Code", book1.getTitle());
+    }
 }
