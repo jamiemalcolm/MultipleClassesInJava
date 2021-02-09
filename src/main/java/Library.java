f30bbe59fc535add1ff7adce50a2b392ede200a2
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-09 18:10:24 +0000

    worked on advanced extenstion got lots of errors

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Library {
     private ArrayList<Book> collectionOfBooks;
     private int capacity;
+    private HashMap<String, Integer> booksByGenre;
 
     public Library(){
         this.collectionOfBooks = new ArrayList<Book>();
         this.capacity = 2;
+        this.booksByGenre = new HashMap<String, Integer>();
     }
 
     public int getNumOfBooksInCollection() {
@@ -16,6 +19,8 @@ public class Library {
     public void addBook(Book book) {
         if(this.getNumOfBooksInCollection() < this.capacity){
             this.collectionOfBooks.add(book);
+            // possible solution for advanced Extension
+//            this.booksByGenre.put(book.getGenre(), this.booksByGenre.get(book.getGenre()) + 1);
         }
     }
 
